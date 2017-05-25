f861ef82cbe4508bb90888f3b172a00485d5183d
vacili4 <Velbkj428174>
2017-05-26 02:24:36 +1000

    база

---
+++ app/src/main/java/ru/gtcraft27/gtcraft/MainActivity.java
@@ -0,0 +1,18 @@
+package ru.gtcraft27.gtcraft;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by vasilich on 26.05.2017.
+ */
+
+public class MainActivity  extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
